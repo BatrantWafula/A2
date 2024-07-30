61f2bd8d0439cc72dde453ecc14054f2baf32f4a
Your Name <your_email@example.com>
2024-07-30 18:19:35 +0300

    Initial commit

--- A2/settings.py
+++ A2/settings.py
@@ -79,6 +79,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'Barry',
+        'PASSWORD': 'Bary54wafula@',
+        'HOST': 'localhost',
+        'PORT': '',
+
     }
 }
 
